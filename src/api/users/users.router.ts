b942c66ac1b0b11cda5812708e63fa0649d53f0c
andrewsixsixsix <a.laptiiev13@gmail.com>
2024-01-06 11:52:12 +0200

    refactor: extract auth logic to a separate service

--- src/api/users/users.router.ts
+++ src/api/users/users.router.ts
@@ -1,14 +1,10 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
-import { login, register } from './users.service.js';
+import { login, logout, register } from './auth.service.js';
 
 export const usersRouterV1 = express.Router();
 export const usersRouterV2 = express.Router();
 
 usersRouterV1.route('/register').post(register);
 usersRouterV1.route('/login').post(login);
-
-usersRouterV1.route('/logout').get((_req: Request, res: Response) => {
-  res.status(200);
-  res.send('Logout');
-});
+usersRouterV1.route('/logout').get(logout);
