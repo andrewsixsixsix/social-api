e4ec0c751cb3421ae67751676b1a0f6a2eef6f1c
andrewsixsixsix <a.laptiiev13@gmail.com>
2024-01-07 20:55:28 +0200

    feat(database): configure mysql database

---
+++ src/config/mysql.config.ts
@@ -0,0 +1,22 @@
+import mysql, { ConnectionOptions } from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const mysqlConfig: ConnectionOptions = {
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  namedPlaceholders: true,
+};
+
+export const pool = mysql.createPool({
+  ...mysqlConfig,
+  waitForConnections: true,
+  connectionLimit: 20,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+});
