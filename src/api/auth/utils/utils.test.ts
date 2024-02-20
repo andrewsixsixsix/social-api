e8e2339d4a89c73de24509258be23f4c607265ca
andrewsixsixsix <a.laptiiev13@gmail.com>
2024-02-20 21:24:17 +0200

    test: password hashing

--- src/api/auth/utils/utils.test.ts
+++ src/api/auth/utils/utils.test.ts
@@ -1,5 +1,15 @@
+// @ts-nocheck
+import { hashPassword } from './hash-password.ts';
+
 describe('Test auth utils', () => {
-  it('true is true', () => {
-    expect(true).toBe(true);
+  it('password is hashed', () => {
+    const password = 'password';
+    const username = 'username';
+
+    const hashed = hashPassword(password, username);
+
+    const expected = '12fc00817ab00b8a18823810a9419cfee4280ff1fd9f77f11cccf32bc22fe498';
+
+    expect(hashed).toBe(expected);
   });
 });
